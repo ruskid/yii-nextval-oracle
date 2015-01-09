0a4ea873d6b3242c00ab60bf52703528570c0188
Victor Demin <demmbox@gmail.com>
2015-01-09 09:52:09 +0100

    Update OracleNextValBehavior.php
    Explanation of "Expanded non LONG bind data supplied after actual LONG or LOB column"

--- OracleNextValBehavior.php
+++ OracleNextValBehavior.php
@@ -44,11 +44,11 @@ class OracleNextValBehavior extends CActiveRecordBehavior {
     }
 
     /**
-     * Quick fix! I get this error If i don't set primary key to any value before save.
+     * Quick fix! If your active record has CLOB attribute then you will get this error.
      * "General error: 24816 OCIStmtExecute: ORA-24816: Expanded non LONG bind data supplied after actual LONG or LOB column"
-     * So the purpose of afterConstruct method is to set the primary key to some value (1) and rewrite it in beforeSave() method.
-     * pretty stupid, but works xd
-     *
+     * Another oracle and pdo_oci stuff... To solve the problem you should bind your clob parameters at the end of bind list.
+     * So the purpose of afterConstruct method is to set the primary key to some value (1) and rewrite it in after maintaining the bind order.
+     *  
      * Responds to {@link CModel::onAfterConstruct} event.
      * Override this method and make it public if you want to handle the corresponding event
      * of the {@link CBehavior::owner owner}.
